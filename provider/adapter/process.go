912558e84e6a7979c7cf061437b75a3a7c79d43c
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-02-08 18:00:09 -0300

    feat(provider/adapter)!: `ProcessToStep` starts the process
    Automatically starts the process if it is not already started
    
    BREAKING CHANGE: the `ProcessToStep` function automatically starts the
    process if it is not already started

--- provider/adapter/process.go
+++ provider/adapter/process.go
@@ -5,19 +5,31 @@ import (
 	"github.com/LucasAVasco/falcula/service/iface"
 )
 
-// ProcessStep wraps a process in a Step interface
+// ProcessStep wraps a process in a Step interface. It automatically starts the process if it is not already started
 type ProcessStep struct {
-	process *process.Process
+	startErr error
+	process  *process.Process
 }
 
-// ProcessToStep converts a process to a Step interface
+// ProcessToStep converts a process to a Step interface. It automatically starts the process if it is not already started
 func ProcessToStep(process *process.Process) iface.Step {
-	return &ProcessStep{
+	p := ProcessStep{
 		process: process,
 	}
+
+	// Starts the process if it is not already started
+	if !process.Started() {
+		p.startErr = process.Start()
+	}
+
+	return &p
 }
 
 func (p *ProcessStep) Wait() (*iface.ExitInfo, error) {
+	if p.startErr != nil {
+		return &iface.ExitInfo{}, p.startErr
+	}
+
 	return p.process.Wait(), nil
 }
 
