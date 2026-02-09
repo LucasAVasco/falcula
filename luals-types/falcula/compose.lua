4bd2da64478a68a4cc261b4bd9fe98ad9ed7bc9e
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-02-09 12:46:55 -0300

    chore(luals-types/falcula/compose): update provider types
    Update signature of the following methods:
    
    - `new_build_service`: complex options, including building
      multi-platform images
    
    - `new_up_service`: support running on a specific platform (emulated)
    
    - `new_push_service`: complex options, including pushing multi-platform
      images

--- luals-types/falcula/compose.lua
+++ luals-types/falcula/compose.lua
@@ -34,27 +34,46 @@ function M.Provider:add_push_image(image) end
 ---@param images string[] List of image names.
 function M.Provider:add_push_images(images) end
 
+---@class FalculaComposeProvider.BuildInfo Information to build a service.
+---@field services? string[] List of services to build. Defaults to all buildable services.
+---@field platforms? string[] List of platforms to build. In the format `os/arch`. Example: `linux/amd64`. Defaults to current platform.
+
+---@class FalculaComposeProvider.BuildServiceOpts Options for the 'Build' service.
+---@field no_pull? boolean Only build the images defined in the compose file (does not pull not required images).
+---@field builds? FalculaComposeProvider.BuildInfo[] List of build information.
+
 ---Create a new 'Build' service.
 ---This service builds the images defined in the compose file.
----@param only_build? boolean Only build the images defined in the compose file (does not pull not required images).
+---@param opts? FalculaComposeProvider.BuildServiceOpts Options for the service.
 ---@return FalculaServiceService
-function M.Provider:new_build_service(only_build) end
+function M.Provider:new_build_service(opts) end
 
 ---Create a new 'Up' service.
 ---This service builds and starts the containers defined in the compose file.
+---@param platform? string Platform to run the containers on.
 ---@return FalculaServiceService
-function M.Provider:new_up_service() end
+function M.Provider:new_up_service(platform) end
 
 ---Create a new 'Down' service.
 ---This service stops and removes the containers defined in the compose file.
 ---@return FalculaServiceService
 function M.Provider:new_down_service() end
 
+---@class FalculaComposeProvider.PushInfo Information to push services images to a registry.
+---@field services? string[] List of services to push. Defaults to all buildable services.
+---@field images? string[] List of images to push. Defaults to all buildable images.
+---@field platforms? string[] List of platforms to push. In the format `os/arch`. Example: `linux/amd64`. Defaults to current platform.
+---@field registries string[] Registries to push the images to.
+---@field tag? string Tag to append to the images in the registry. Example: if tag is 'v1.0.0', the image will be pushed as 'image:v1.0.0'.
+
+---@class FalculaComposeProvider.PushServiceOpts Options for the 'Push' service.
+---@field pushes FalculaComposeProvider.PushInfo[] List of push information.
+
 ---Create a new 'Push' service.
 ---This service pushes the images defined in the compose file to a registry.
 ---You must be logged in to the registry before using this service.
----@param registry string Registry to push the images to.
+---@param opts FalculaComposeProvider.PushServiceOpts Options for the service.
 ---@return FalculaServiceService
-function M.Provider:new_push_service(registry) end
+function M.Provider:new_push_service(opts) end
 
 return M
