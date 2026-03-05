56dcd95dd718b0e67578e0df3d206a260906d2f8
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-03-04 23:26:32 -0300

    chore(luals-types/falcula/compose): provide opts to provider and service
    Update the types of the options used to create the provider. Include the
    default provider options to it
    
    Update the types of the methods used to create new services to include a
    service options argument. This argument overrides the default options of
    the created service

--- luals-types/falcula/compose.lua
+++ luals-types/falcula/compose.lua
@@ -3,7 +3,7 @@
 ---@class FalculaCompose Docker/Podman compose module.
 local M = {}
 
----@class (exact) FalculaComposeOpts Docker/Podman compose service.
+---@class (exact) FalculaComposeOpts: FalculaServiceProviderOpts Docker/Podman compose service.
 ---@field push_images? string[] List of images that the 'Push' service will push.
 
 ---@class FalculaComposeProvider Service provider for Docker/Podman compose service.
@@ -27,18 +27,20 @@ function M.Provider:get_name() end
 
 ---Add an image to push when using the 'Push' service.
 ---@param image string Image name.
-function M.Provider:add_push_image(image) end
+---@param opts? FalculaServiceServiceOpts Options for the service.
+function M.Provider:add_push_image(image, opts) end
 
 ---Add images to push when using the 'Push' service.
 ---Equivalent to calling `add_push_image` multiple times.
 ---@param images string[] List of image names.
-function M.Provider:add_push_images(images) end
+---@param opts? FalculaServiceServiceOpts Options for the service.
+function M.Provider:add_push_images(images, opts) end
 
----@class FalculaComposeProvider.BuildInfo Information to build a service.
+---@class (exact) FalculaComposeProvider.BuildInfo Information to build a service.
 ---@field services? string[] List of services to build. Defaults to all buildable services.
 ---@field platforms? string[] List of platforms to build. In the format `os/arch`. Example: `linux/amd64`. Defaults to current platform.
 
----@class FalculaComposeProvider.BuildServiceOpts Options for the 'Build' service.
+---@class (exact) FalculaComposeProvider.BuildServiceOpts: FalculaServiceServiceOpts Options for the 'Build' service.
 ---@field no_pull? boolean Only build the images defined in the compose file (does not pull not required images).
 ---@field builds? FalculaComposeProvider.BuildInfo[] List of build information.
 
@@ -52,21 +54,23 @@ function M.Provider:new_build_service(opts) end
 ---This service builds and starts the containers defined in the compose file.
 ---@param platform? string Platform to run the containers on.
 ---@return FalculaServiceService
-function M.Provider:new_up_service(platform) end
+---@param opts? FalculaServiceServiceOpts Options for the service.
+function M.Provider:new_up_service(platform, opts) end
 
 ---Create a new 'Down' service.
 ---This service stops and removes the containers defined in the compose file.
 ---@return FalculaServiceService
-function M.Provider:new_down_service() end
+---@param opts? FalculaServiceServiceOpts Options for the service.
+function M.Provider:new_down_service(opts) end
 
----@class FalculaComposeProvider.PushInfo Information to push services images to a registry.
+---@class (exact) FalculaComposeProvider.PushInfo Information to push services images to a registry.
 ---@field services? string[] List of services to push. Defaults to all buildable services.
 ---@field images? string[] List of images to push. Defaults to all buildable images.
 ---@field platforms? string[] List of platforms to push. In the format `os/arch`. Example: `linux/amd64`. Defaults to current platform.
 ---@field registries string[] Registries to push the images to.
 ---@field tag? string Tag to append to the images in the registry. Example: if tag is 'v1.0.0', the image will be pushed as 'image:v1.0.0'.
 
----@class FalculaComposeProvider.PushServiceOpts Options for the 'Push' service.
+---@class (exact) FalculaComposeProvider.PushServiceOpts: FalculaServiceServiceOpts Options for the 'Push' service.
 ---@field pushes FalculaComposeProvider.PushInfo[] List of push information.
 
 ---Create a new 'Push' service.
