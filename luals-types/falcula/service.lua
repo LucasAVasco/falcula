8ef8851cb163c6150108c92d24d6e728a5b32fb3
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-03-04 23:18:36 -0300

    chore(luals-types/falcula/service): add options types
    Add provider and service options types

--- luals-types/falcula/service.lua
+++ luals-types/falcula/service.lua
@@ -3,7 +3,14 @@
 ---@class FalculaService Service type definitions.
 local M = {}
 
----@class FalculaServiceService Service implementation.
-M.Service = {}
+---@class FalculaServiceServiceOpts Service options.
+---@field start_disabled? boolean If the service should not be automatically started. The user must enable the service manually.
+
+---@class FalculaServiceService Generic service.
+
+---@class FalculaServiceProviderOpts Provider options.
+---@field service_opts? FalculaServiceServiceOpts Default options for the generated services.
+
+---@class FalculaServiceProvider Generic provider.
 
 return M
