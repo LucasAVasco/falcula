e9aa874fda2a729622615a76a7acfbdab681b3d4
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2026-03-04 23:04:10 -0300

    feat(provider/base): provider and service accept config and opts
    The user can configure the provider and the service with a configuration
    and options structures

--- provider/base/provider.go
+++ provider/base/provider.go
@@ -4,39 +4,49 @@ package base
 import (
 	"github.com/LucasAVasco/falcula/colorgen"
 	"github.com/LucasAVasco/falcula/multiplexer"
+	"github.com/LucasAVasco/falcula/service/iface"
 
 	"github.com/fatih/color"
 )
 
-// Provider represents a provider. Provides the basic data required by a provider. Any provider should inherit from this
-type Provider struct {
-	Multiplexer *multiplexer.Multiplexer
-	Name        string
-	Color       *color.Color
+// ProviderConfig is the configuration for a provider. It is required to create a provider
+type ProviderConfig struct {
+	Multiplexer        *multiplexer.Multiplexer
+	Name               string
+	DefaultServiceOpts iface.Opts // The default options for a service. If not provided, a default value will be used
 }
 
-func NewProvider(multi *multiplexer.Multiplexer, name string) *Provider {
+// Provider represents a provider. Provides the basic data required by a provider. Any provider should inherit from this
+type Provider struct {
+	Color  *color.Color
+	Config *ProviderConfig
+}
+
+func NewProvider(config *ProviderConfig) *Provider {
 	return &Provider{
-		Multiplexer: multi,
-		Name:        name,
-		Color:       colorgen.Next(),
+		Color:  colorgen.Next(),
+		Config: config,
 	}
 }
 
 func (p *Provider) GetName() string {
-	return p.Name
+	return p.Config.Name
 }
 
 // NewService creates a new service. Its name is automatically prefixed with the provider name. If empty, the provider name is used
-func (p *Provider) NewService(name string) *Service {
+func (p *Provider) NewService(name string, opts *ServiceOpts) *Service {
 	if name == "" {
-		name = p.Name
+		name = p.Config.Name
 	} else {
-		name = p.Name + "." + name
+		name = p.Config.Name + "." + name
 	}
 
-	return &Service{
-		Provider: p,
-		Name:     name,
+	config := ServiceConfig{
+		Multiplexer: p.Config.Multiplexer,
+		Color:       p.Color,
+		Name:        name,
+		Opts:        p.Config.DefaultServiceOpts,
 	}
+
+	return NewService(&config, opts)
 }
--- provider/base/service.go
+++ provider/base/service.go
@@ -1,22 +1,51 @@
 package base
 
-import "github.com/LucasAVasco/falcula/process"
+import (
+	"github.com/LucasAVasco/falcula/multiplexer"
+	"github.com/LucasAVasco/falcula/process"
+	"github.com/LucasAVasco/falcula/service/iface"
+	"github.com/fatih/color"
+)
+
+// ServiceConfig is a structure that holds the configurations for a service. It is required to create a service
+type ServiceConfig struct {
+	Multiplexer *multiplexer.Multiplexer
+	Color       *color.Color
+	Name        string
+	Opts        iface.Opts
+}
+
+// ServiceOpts is a structure that holds the options for a service. It is optional
+type ServiceOpts struct {
+	StartDisabled *bool
+}
 
 // Service represents a base service with the basic data required by a service. Any service should inherit from this
 type Service struct {
-	Provider *Provider
-	Name     string
+	Config ServiceConfig
 }
 
-func NewService(provider *Provider, name string) *Service {
-	return &Service{
-		Provider: provider,
-		Name:     provider.Name + "." + name,
+// NewService creates a new service. The config is required and the opts is optional
+func NewService(config *ServiceConfig, opts *ServiceOpts) *Service {
+	s := Service{
+		Config: *config,
 	}
+
+	if opts != nil {
+		if opts.StartDisabled != nil {
+			s.Config.Opts.StartDisabled = *opts.StartDisabled
+		}
+	}
+
+	return &s
 }
 
 func (s *Service) GetName() string {
-	return s.Name
+	return s.Config.Name
+}
+
+func (s *Service) GetOpts() *iface.Opts {
+	return &s.Config.Opts
 }
 
 // NewProcessOptions returns a new process.Options struct configured for the service. It will use the provider multiplexer and color.
@@ -25,10 +54,10 @@ func (s *Service) GetName() string {
 // manually start the process because they automatically starts the process if it is not already started
 func (s *Service) NewProcessOptions() *process.Options {
 	return &process.Options{
-		Multiplexer: s.Provider.Multiplexer,
-		Name:        s.Name,
+		Multiplexer: s.Config.Multiplexer,
+		Name:        s.Config.Name,
 		Wait:        false,
 		ManualStart: true,
-		Color:       s.Provider.Color,
+		Color:       s.Config.Color,
 	}
 }
